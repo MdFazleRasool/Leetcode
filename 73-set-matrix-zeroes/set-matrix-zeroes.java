157a89449f98251b2fe4385f1208c22c04a03e3c
MdFazleRasool <163381172+MdFazleRasool@users.noreply.github.com>
2025-12-20 21:36:26 +0530

    Time: 1 ms (61.48%) | Memory: 47.4 MB (18.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,25 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length , n = matrix[0].length   ;
-        int copy[][] = new int [m][n]; 
-        for(int i =0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                copy[i][j] = matrix[i][j] ;
-            }
-        }
+        ArrayList<Integer> al = new ArrayList<>();
         for(int i =0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    for(int a = 0; a<n;a++) copy[i][a] = 0;
-                    for(int a = 0; a<m;a++) copy[a][j] = 0;
+                    al.add(i);
+                    al.add(j);
                 }
             }
         }
-        for(int i =0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                matrix[i][j] = copy[i][j] ;
-            }
+        for(int k = 0;k<al.size();k+=2){
+            int i = al.get(k), j = al.get(k+1);
+            for(int a = 0; a<n;a++) matrix[i][a] = 0;
+            for(int a = 0; a<m;a++) matrix[a][j] = 0;
         }
-        
+
+        // for(int a = 0; a<n;a++) copy[i][a] = 0;
+        // for(int a = 0; a<m;a++) copy[a][j] = 0;
     }
 }
