cf5c353f570a4968713d5b136de76846468a090a
MdFazleRasool <163381172+MdFazleRasool@users.noreply.github.com>
2025-12-20 23:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (34.34%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,19 +1,25 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length , n = matrix[0].length   ;
-        ArrayList<Integer> al = new ArrayList<>();
+        boolean row[] = new boolean[m];
+        boolean col[] = new boolean[n];
         for(int i =0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    al.add(i);
-                    al.add(j);
+                    row[i] =true;
+                    col[j] = true;
                 }
             }
         }
-        for(int k = 0;k<al.size();k+=2){
-            int i = al.get(k), j = al.get(k+1);
-            for(int a = 0; a<n;a++) matrix[i][a] = 0;
-            for(int a = 0; a<m;a++) matrix[a][j] = 0;
+        for(int k = 0;k<m;k++){
+            if(row[k] == true){
+                for(int a = 0; a<n;a++) matrix[k][a] = 0;
+            }
+        }
+        for(int k = 0;k<n;k++){
+            if(col[k] == true){
+                for(int a = 0; a<m;a++) matrix[a][k] = 0;
+            }
         }
 
         // for(int a = 0; a<n;a++) copy[i][a] = 0;
