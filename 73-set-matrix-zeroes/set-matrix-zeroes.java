33855ed30e10b8f1d075a04fb65ce6d23e75020e
MdFazleRasool <163381172+MdFazleRasool@users.noreply.github.com>
2025-12-20 23:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (6.95%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length , n = matrix[0].length   ;
-        boolean row[] = new boolean[m];
-        boolean col[] = new boolean[n];
-        for(int i =0;i<m;i++){
-            for(int j = 0;j<n;j++){
+        boolean row = false;
+        boolean col = false;
+        for(int i = 0 ;i<m;i++){
+            if(matrix[i][0] == 0) col = true;
+        }
+        for(int i = 0 ;i<n;i++){
+            if(matrix[0][i] == 0) row = true;
+        }
+        for(int i =1;i<m;i++){
+            for(int j = 1;j<n;j++){
                 if(matrix[i][j] == 0){
-                    row[i] =true;
-                    col[j] = true;
+                    matrix[i][0] =0;
+                    matrix[0][j] =0;
                 }
             }
         }
-        for(int k = 0;k<m;k++){
-            if(row[k] == true){
-                for(int a = 0; a<n;a++) matrix[k][a] = 0;
-            }
-        }
-        for(int k = 0;k<n;k++){
-            if(col[k] == true){
-                for(int a = 0; a<m;a++) matrix[a][k] = 0;
-            }
-        }
 
-        // for(int a = 0; a<n;a++) copy[i][a] = 0;
-        // for(int a = 0; a<m;a++) copy[a][j] = 0;
+        for(int i = 1 ;i<m;i++){
+            if(matrix[i][0] == 0) {
+                for(int a = 0; a<n;a++) matrix[i][a] = 0;
+            }
+        }
+        for(int i = 1 ;i<n;i++){
+            if(matrix[0][i] == 0) {
+                for(int a = 0; a<m;a++) matrix[a][i] = 0;
+            }
+        }
+        if(row)  for(int a = 0; a<n;a++) matrix[0][a] = 0; 
+        
+        if(col)  for(int a = 0; a<m;a++) matrix[a][0] = 0;
     }
 }
