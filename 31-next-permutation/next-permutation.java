ab2a655269aface4047b59d2a9d2077b036b849c
MdFazleRasool <163381172+MdFazleRasool@users.noreply.github.com>
2025-12-20 19:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (33.48%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,4 +1,13 @@
 class Solution {
+    public void reverse(int arr[], int  l , int r){
+        while(l<r){
+            int temp = arr[l];
+            arr[l]= arr[r];
+            arr[r]= temp;
+            l++;
+            r--;
+        }
+    }
     public void nextPermutation(int[] arr) {
         int n = arr.length -1 , pivot=-1;
         if(arr.length==1) return;
@@ -9,14 +18,8 @@ class Solution {
             }
         }
         if (pivot == -1) {
-            int l = pivot+1 , r = n;
-            while(l<r){
-                int temp = arr[l];
-                arr[l]= arr[r];
-                arr[r]= temp;
-                l++;
-                r--;
-            }
+            reverse(arr,pivot+1 ,n);
+            
             return;
         }
         
@@ -32,15 +35,7 @@ class Solution {
 
         
         // rotating 
+        reverse(arr,pivot+1 ,n);
         
-        int l = pivot+1 ;
-        int r = n;
-        while(l<r){
-            temp = arr[l];
-            arr[l]= arr[r];
-            arr[r]= temp;
-            l++;
-            r--;
-        }
     }
 }
